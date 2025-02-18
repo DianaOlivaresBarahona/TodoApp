8953c3b9b309ec41bfc05ffa6c37e742028465ad
DianaOlivaresBarahona <olivaresbarahona@live.se>
2025-02-18 13:39:13 +0100

    Fixade missen med import todo.css

--- src/components/todo/ToDo.jsx
+++ src/components/todo/ToDo.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Todolist from "../todolist/Todolist";
 import Todoform from "../todoform/Todoform";
 
-import "./ToDo.css";
-
 const ToDo = ({
   list,
   setList,
